82c5840d907e1db1d9cb5991fbb77b71f5b70141
Joseph Madras <josephkabemba77@gmail.com>
2021-12-06 03:13:47 +0100

    style: create scss colors variables

--- src/components/TimeCard/TimeCard.jsx
+++ src/components/TimeCard/TimeCard.jsx
@@ -2,22 +2,22 @@ import "./TimeCard.scss";
 import ThreeDots from "../../images/icon-ellipsis.svg";
 
 export const TimeCard = ({ info, period }) => {
-  const { title,  timeframes} = info;
+  const { title, timeframes } = info;
 
- return (
-    <div cla>
-      <div>
+  return (
+    <div className="card">
+      <div className="card__title">
         <div>{title}</div>
         <div>
           <img src={ThreeDots} alt="Three dots" />
         </div>
       </div>
 
-      <div>
+      <div className="card__current">
         <h1>{timeframes.daily.current}hrs</h1>
       </div>
 
-      <div>
+      <div className="card__previous">
         Last {period} - {timeframes.daily.previous}hrs
       </div>
     </div>
